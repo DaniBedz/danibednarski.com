a2215e8a095e7733000801a179960ab5c991d4ce
Dani Bednarski <49581958+DaniBedz@users.noreply.github.com>
2021-09-28 14:07:44 +1000

    Fix padding on resume route

--- src/routes/resume/index.svelte
+++ src/routes/resume/index.svelte
@@ -1,11 +1,22 @@
-<main>
+<script>
+    import { isNavOpen } from '$lib/core';
+
+    let showMenu;
+
+    isNavOpen.subscribe((value) => {
+        showMenu = value;
+    });
+</script>
+
+
+<main style={ showMenu ? 'margin-left: 1rem' : 'margin-left: 0' }>
     <iframe title="resume" width="100%" src="/resume.pdf" />
 </main>
 
 <style>
     main {
         display: flex;
-        margin-left: 1rem;
+        /* margin-left: 1rem; */
     }
     iframe {
         border: none;
