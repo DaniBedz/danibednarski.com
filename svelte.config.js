a5d4102f49fb24744c2172155a8724d887bc61f1
Dani Bednarski <49581958+DaniBedz@users.noreply.github.com>
2021-09-28 22:30:14 +1000

    Add preprocess to build step.

--- svelte.config.js
+++ svelte.config.js
@@ -2,11 +2,11 @@ import mdsvexConfig from './mdsvex.config.js';
 
 import adapter from '@sveltejs/adapter-cloudflare-workers';
 import { mdsvex } from 'mdsvex';
-
+import preprocess from 'svelte-preprocess';
 
 const config = {
     extensions: ['.svelte', ...mdsvexConfig.extensions],
-    preprocess: [mdsvex(mdsvexConfig)],
+    preprocess: [preprocess(), mdsvex(mdsvexConfig)],
     kit: {
         target: '#svelte',
         adapter: adapter(),
