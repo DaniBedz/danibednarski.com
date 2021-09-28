c37fb480e9c91739fb9805df9c82632d37c1c16a
Dani Bednarski <49581958+DaniBedz@users.noreply.github.com>
2021-09-28 16:48:47 +1000

    Apply theme to resume

--- src/routes/resume/index.svelte
+++ src/routes/resume/index.svelte
@@ -1,24 +1,30 @@
 <script>
+    import { onMount } from 'svelte';
+
     import { isNavOpen } from '$lib/core';
 
-    let showMenu;
+    let Resume;
+    onMount(async () => {
+        const module = await import('$lib/Resume.svelte');
+        Resume = module.default;
+    });
+
+    $: showMenu = true;
 
     isNavOpen.subscribe((value) => {
         showMenu = value;
     });
 </script>
 
-
-<main style={ showMenu ? 'margin-left: 1rem' : 'margin-left: 0' }>
-    <iframe title="resume" width="100%" src="/resume.pdf" />
+<main style={ showMenu ? 'margin-left: 1rem' : 'margin-left: 0rem' }>
+    <svelte:component this={ Resume } />
 </main>
 
 <style>
     main {
-        display: flex;
-        /* margin-left: 1rem; */
-    }
-    iframe {
-        border: none;
+        display: grid;
+        position: relative;
+        align-content: baseline;
+        justify-content: center;
     }
 </style>
